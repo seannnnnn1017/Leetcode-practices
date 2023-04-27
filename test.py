29b52dc4b4cb2a3e6529cb58e28a0be855347738
seannnnnn1017 <op23756778@gmail.com>
2023-04-28 00:53:33 +0800

    123

--- test.py
+++ test.py
@@ -5,26 +5,26 @@ class Solution:
                                 [2,4],
                                 [3,1,2],
                                 [1,3,1,1]]) -> int:
-        #�Ыؤ@�ӪŦr��A�ΨӰO���j����t��m���X�{����
+        #創建一個空字典，用來記錄磚塊邊緣位置的出現次數
         arr = {}
-        # �O���ثe�X�{���Ƴ̦h���j����t��m���X�{����
+        # 記錄目前出現次數最多的磚塊邊緣位置的出現次數
         mx=0
-        # ���C�@��A�M�����F�̫�@�ӿj�����~���C�ӿj��
+        # 對於每一行，遍歷除了最後一個磚塊之外的每個磚塊
         for i in range(len(A)):
             for j in range(len(A[i])-1):
-                # �N�ӿj�����e�ץ[�W�e�@�ӿj�����e��
+                # 將該磚塊的寬度加上前一個磚塊的寬度
                 if j!=0:
                     A[i][j]+=A[i][j-1]
-                # �p�G�ӿj�����k��t��m�S���b�r�夤�X�{�L�A�h�N���l�Ƭ� 0
+                # 如果該磚塊的右邊緣位置沒有在字典中出現過，則將其初始化為 0
                 if A[i][j]-1 not in arr:
                     arr[A[i][j]-1]=0
-                # �N�Ӧ�m���X�{���ƥ[ 1
+                # 將該位置的出現次數加 1
                 arr[A[i][j]-1]+=1
-                # ��s�X�{���Ƴ̦h���j����t��m���X�{����
+                # 更新出現次數最多的磚塊邊緣位置的出現次數
                 mx = max(mx,arr[A[i][j]-1])
                 print(arr)
-        # ��^���`���״�h�X�{���Ƴ̦h���j����t��m���X�{����
+        # 返回牆的總高度減去出現次數最多的磚塊邊緣位置的出現次數
         print(len(A)-mx)
         return len(A)-mx
 
-Solution.leastBricks(None,[[100000000,100000000],[100000000,100000000]])
+Solution.leastBricks(None)
