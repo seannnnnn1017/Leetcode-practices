4e1e2a5b709d480c350450c109bdfc3565216912
seannnnnn1017 <op23756778@gmail.com>
2023-05-05 00:53:53 +0800

    1

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -10,5 +10,19 @@ class Solution:
                 if other[i] != ch:
                     return shortest[:i]
         return shortest 
+    
+strs5=["abca","aba","aaab"]
+def longestCommonPrefix(self, strs):
+        k=''
+        i=0
+        if len(strs) <2:
+            return strs[0]
+        for i in range (len(min(strs,key=len))+1):
+            for j in  range(len(strs)):
+                if strs[0][0:i] != strs[j][0:i] :
+                    return strs[0][:i-1]
 
-print('a',Solution.longestCommonPrefix(1,["reflower","flow","flight"]))
+        return min(strs,key=len)
+
+
+print(longestCommonPrefix(1,strs5))
