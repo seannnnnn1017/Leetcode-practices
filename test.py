73bc9a671d000f0fed6ba2b7d94cc93304f27abb
seannnnnn1017 <op23756778@gmail.com>
2023-05-04 21:37:20 +0800

    4

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
 class Solution:
     def letterCombinations(self, digits='23'):
-        if not digits:
-            return []
+        if digits=='':return []
         L=[]
         map = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-
-        def com(combination,digit):
-            if not digit:
-                return L.append(combination)
-                
-            digit_str=map[digit[0]]
-            for letter in digit_str:
-                com(combination+letter,digit[1:])
+        def com(combination,digits):
+            if not digits:return L.append(combination)
+            digits_str=map[digits[0]]
+            for letter in digits_str:
+                print(combination+letter)
+                com(combination+letter,digits[1:])
             return L
-        return com("",digits)
-
-Solution.letterCombinations(1)
+        return com('',digits)
+print(Solution.letterCombinations(1))
